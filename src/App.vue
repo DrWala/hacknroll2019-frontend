b6bf003fcc36ce6e8ee9ed6796143df84ebff3a1
Yash Gadodia <pirsquare.yash@gmail.com>
2020-01-18 23:54:17 +0800

    add planet-chart

--- src/App.vue
+++ src/App.vue
@@ -6,43 +6,92 @@
     {{my_variable}}
     {{my_questions}}
     {{my_answer}}
-    <BarChart />
+    <canvas id="stat-chart"></canvas>
   </div>
 </template>
 
 <script>
+import Chart from "chart.js";
+import planetChartData from "./planet-chart.js";
 
-import BarChart  from './components/BarChart'
 export default {
-  name: 'app',
-  components: {
-    'BarChart': BarChart,
-  },
+  name: "app",
   data() {
     return {
       my_variable: "hello!",
       my_questions: null,
       my_answer: "test"
-    }
+    };
   },
-  mounted() { //to implement the method we implemented below
-    // this.renderChart(data, options)
-    // this.getData()
-    // this.getUserQuestion()
-    // this.getUserAnswer()
+  mounted() {
+    //to implement the method we implemented below
+    this.getData();
+    this.getUserQuestion();
+    this.getUserAnswer();
+    // construct what you think data should look like for your chart
+    let mythischart = {
+      type: "line",
+      data: {
+        labels: [ // change this
+          "Mercury",
+          "Venus",
+          "Earth",
+          "Mars",
+          "Jupiter",
+          "Saturn",
+          "Uranus",
+          "Neptune"
+        ],
+        datasets: [
+          {
+            //change this
+            label: "Number of Moons",
+            data: [3, 0, 1, 2, 67, 62, 27, 14]
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        lineTension: 1,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                padding: 25
+              }
+            }
+          ]
+        }
+      }
+    };
+    this.createChart("stat-chart", mythischart);
   },
   methods: {
-    getData() { //return dob and gender
-      this.$database.get_user(1, d => this.my_variable = d.val())
+    getData() {
+      //return dob and gender
+      this.$database.get_user(1, d => (this.my_variable = d.val()));
     },
     getUserQuestion() {
-      this.$database.get_user_question_by_id(1,1,d => this.my_questions = d.val())
+      this.$database.get_user_question_by_id(
+        1,
+        1,
+        d => (this.my_questions = d.val())
+      );
     },
     getUserAnswer() {
-      this.$database.get_user_answers(1, d => this.my_answer = d.val())
+      this.$database.get_user_answers(1, d => (this.my_answer = d.val()));
+    },
+    createChart(chartId, chartData) {
+      const ctx = document.getElementById(chartId);
+      const myChart = new Chart(ctx, {
+        type: chartData.type,
+        data: chartData.data,
+        options: chartData.options
+      });
     }
   }
-}
+};
 </script>   
 
 <style>

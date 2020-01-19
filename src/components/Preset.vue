5cef5fd77dd1495b66466dda525c892ff2abaf2d
Azeem Vasanwala <azeem.iron@gmail.com>
2020-01-19 10:56:18 +0800

    merge

--- src/components/Preset.vue
+++ src/components/Preset.vue
@@ -1,7 +1,16 @@
 <template>
     <div id="app">
         <h1>Preset questions</h1>
-        <canvas id="scat-chart"></canvas>
+        <form id="form" v-on:submit.prevent="onSubmit">
+            <div v-for="(question, id) in questions" :key="id">
+                <label :for="question.question">{{ question.question }}</label>
+                <input type="checkbox" v-model="user_questions" :value="id" />
+                <!-- {{ id }}---{{ question }} -->
+            </div>
+            <button>
+                Submit
+            </button>
+        </form>
     </div>
 </template>
 
@@ -11,11 +20,28 @@ export default {
     name: "app",
     data() {
         return {
+            form: {},
+            user_id: null,
+            questions: null,
+            user_questions: []
         };
     },
     mounted() {
+        let uri = window.location.search.substring(1);
+        let params = new URLSearchParams(uri);
+        this.user_id = params.get("user_id");
+        this.getPresetQuestions();
     },
     methods: {
+        getPresetQuestions() {
+            this.$database.get_preset_questions(d => {
+                this.questions = d.val();
+            });
+        },
+        onSubmit(){
+            console.log(this.user_questions)
+            this.$database.set_user_questions(this.user_id, this.user_questions);
+        }
     }
 };
 </script>
@@ -28,5 +54,9 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+}
+#form {
+    margin: 10% 20%;
+    text-align: left;
 }
 </style>
